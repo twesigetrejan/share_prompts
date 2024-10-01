bd34a3dfc00fc1b99c074d235f104c8e5f7de89d
twesigetrejan <trejantwesige19@gmail.com>
2024-10-01 18:20:12 +0300

    auth

--- app/layout.js
+++ app/layout.js
@@ -21,15 +21,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Navbar/>
-          {children}
 
-        </main>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Navbar />
+            {children}
+
+          </main>
+
+        </Provider>
 
       </body>
     </html>
