f10001547929137b881dfc57febd366c7571823a
twesigetrejan <trejantwesige19@gmail.com>
2024-10-02 20:14:46 +0300

    auth almost done

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Nav";
-
+import Provider from "@/components/Provider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
