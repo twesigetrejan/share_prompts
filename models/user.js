038c760e80938c9926c1c3c39a09c79c28c23868
twesigetrejan <trejantwesige19@gmail.com>
2024-10-04 15:38:30 +0300

    google auth

--- models/user.js
+++ models/user.js
@@ -4,11 +4,11 @@ const UserSchema = new Schema({
   email: {
     type: String,
     unique: [true, 'Email already exists!'],
-    required: [true, 'Email is required to sign in please!'],
+    required: [true, 'Email is required to sign in!'],
   },
   username: {
     type: String,
-    required: [true, 'Username is required to sign in please!'],
+    required: [true, 'Username is required to sign in!'],
     match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
   },
   image: {
